a68047b47fd41bda13a1fb682f5473c9b3902f87
Amit Sharma <iamamitshrma@gmail.com>
2024-01-13 18:29:07 +0530

    register user and user modal

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 
-router.route("/register").post(registerUser)
+router.route("/register").post(upload.fields([{name: "avatar", maxCount: 1}, {name: "coverImage", maxCount: 1 }]), registerUser)
 router.route("/login", loginUser)
 
 export default router;
