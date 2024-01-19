e605db87741e85ea14e9dd12417809e2e7b25ae2
Amit Sharma <iamamitshrma@gmail.com>
2024-01-19 22:33:09 +0530

    fix bugs

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -15,16 +15,9 @@ const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/").post(createPlaylist)
-
-router
-    .route("/:playlistId")
-    .get(getPlaylistById)
-    .patch(updatePlaylist)
-    .delete(deletePlaylist);
-
+router.route("/:playlistId").get(getPlaylistById).patch(updatePlaylist).delete(deletePlaylist);
 router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
 router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
-
 router.route("/user/:userId").get(getUserPlaylists);
 
 export default router
